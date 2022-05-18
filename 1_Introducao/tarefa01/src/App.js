fa7bc464eacd13072195cd5a2d104f79d2d157ec
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2022-05-18 14:51:53 -0300

    Tarefa 01, incluindo tags no App.js e estilizando no App.css

---
+++ 1_Introducao/tarefa01/src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Tarefa 01</h1>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eligendi eius earum, cupiditate molestias tempora pariatur 
+         eveniet aspernatur. Eos, rem voluptatem magnam nobis ut quos esse possimus, repellendus ea ratione in!
+         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eligendi eius earum, cupiditate molestias tempora pariatur 
+         eveniet aspernatur. Eos, rem voluptatem magnam nobis ut quos esse possimus, repellendus ea ratione in!
+         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eligendi eius earum, cupiditate molestias tempora pariatur 
+         eveniet aspernatur. Eos, rem voluptatem magnam nobis ut quos esse possimus, repellendus ea ratione in!
+         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eligendi eius earum, cupiditate molestias tempora pariatur 
+         eveniet aspernatur. Eos, rem voluptatem magnam nobis ut quos esse possimus, repellendus ea ratione in!
+         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eligendi eius earum, cupiditate molestias tempora pariatur 
+         eveniet aspernatur. Eos, rem voluptatem magnam nobis ut quos esse possimus, repellendus ea ratione in!
+         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eligendi eius earum, cupiditate molestias tempora pariatur 
+         eveniet aspernatur. Eos, rem voluptatem magnam nobis ut quos esse possimus, repellendus ea ratione in!</p>
+    </div>
+  );
+}
+
+export default App;
