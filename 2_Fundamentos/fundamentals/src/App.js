d9546198f8b95bce2d914045aba16a2ba72079a9
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2022-05-19 16:10:16 -0300

    Criando, exportando e importando componente

---
+++ 2_Fundamentos/fundamentals/src/App.js
@@ -0,0 +1,15 @@
+// Components
+import FirstComponent from './components/FirstComponent';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Components</h1>
+      <FirstComponent/>
+    </div>
+  );
+}
+
+export default App;
