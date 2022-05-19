b668723a7a7917527250052a75f4e9f5ce01f8d0
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2022-05-19 16:39:31 -0300

    Comentários com JSX

--- 2_Fundamentos/fundamentals/src/App.js
+++ 2_Fundamentos/fundamentals/src/App.js
@@ -4,8 +4,13 @@ import FirstComponent from './components/FirstComponent';
 import './App.css';
 
 function App() {
+  // Comentário de apenas uma linha
+  /*
+     Comentário de multiplas linhas
+  */
   return (
     <div className="App">
+      {/* Comentário do JSX*/}
       <h1>Components</h1>
       <FirstComponent/>
     </div>
