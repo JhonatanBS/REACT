b93d75fef5a5841af58904aedbf922a50674c95d
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2022-05-21 15:41:28 -0300

    Adicionando imagens com a tag img

---
+++ 3_Avancando/advancing/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Avançando em REACT</h1>
+      <div>
+        <img src="/img1.jpg" alt="Paisagem" />
+      </div>
+    </div>
+  );
+}
+
+export default App;
