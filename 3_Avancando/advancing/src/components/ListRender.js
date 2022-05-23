1375b6b5ba655327a6477e65aa202f998ecf7928
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2022-05-23 15:23:47 -0300

    key para identificar a chave ou index de um objeto ou array

--- 3_Avancando/advancing/src/components/ListRender.js
+++ 3_Avancando/advancing/src/components/ListRender.js
@@ -3,14 +3,31 @@ import { useState } from "react";
 const ListRender = ()=>{
     const [list] = useState(["Joana", "Lucas", "Paulo", "Matheus"]);
 
+    const [users] = useState([
+      {id: 1, name: "Jhonatan", age: 29},
+      {id: 2, name: "Paulo", age: 25},
+      {id: 3, name: "Lucas", age: 40},
+      {id: 4, name: "Marcos", age: 18},
+    ]);
+
     return(
         <div>
           <ul>
-            {list.map((item)=>(
-                <li>{item}</li>
+            {list.map((item, i)=>(
+                <li key={i}>{item}</li>
             ))}
           </ul>
+
+          <div>
+            <ul>
+              {users.map((user)=>(
+                <li key={user.id}>{user.name} - {user.age}</li>
+              ))}
+            </ul>
+          </div>
         </div>
+
+        
     )
 }
 
