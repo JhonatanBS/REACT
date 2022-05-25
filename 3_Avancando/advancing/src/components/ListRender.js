e6b6953c50bc7b525f55f1b45dc63f76a9f9c696
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2022-05-24 21:11:15 -0300

    Previous state

--- 3_Avancando/advancing/src/components/ListRender.js
+++ 3_Avancando/advancing/src/components/ListRender.js
@@ -3,12 +3,21 @@ import { useState } from "react";
 const ListRender = ()=>{
     const [list] = useState(["Joana", "Lucas", "Paulo", "Matheus"]);
 
-    const [users] = useState([
+    const [users,setUsers] = useState([
       {id: 1, name: "Jhonatan", age: 29},
       {id: 2, name: "Paulo", age: 25},
       {id: 3, name: "Lucas", age: 40},
       {id: 4, name: "Marcos", age: 18},
     ]);
+
+    const deleteRandom = ()=>{
+      const randomNumber = Math.floor(Math.random() * 5);
+      console.log(randomNumber);
+
+      setUsers((prevUsers)=>{
+        return prevUsers.filter((user)=> randomNumber !== user.id);
+      })
+    }
 
     return(
         <div>
@@ -25,9 +34,8 @@ const ListRender = ()=>{
               ))}
             </ul>
           </div>
+          <button onClick={deleteRandom}>Clique para remover</button>
         </div>
-
-        
     )
 }
 
