6decffc42d58ad7eb8e8a701dbd90c7d2fcdc6b7
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2022-05-24 21:29:38 -0300

    Operador ternário

--- 3_Avancando/advancing/src/components/ConditionalRender.js
+++ 3_Avancando/advancing/src/components/ConditionalRender.js
@@ -2,11 +2,25 @@ import { useState } from "react"
 
 const ConditionalRender = () => {
     const [x] = useState(true);
+    const [name] = useState("Jhonatan");
+
   return (
     <div>
         <h1>Isto será exibido!</h1>
         {x && <p>Se x for verdadeiro</p>}
         {!x && <p>Se x for falso</p>}
+
+        <h1>If ternário</h1>
+
+        {name === "Jhonatan" ? (
+            <div>
+                <p>Meu nome é Jhonatan</p>
+            </div>
+        ) : (
+            <div>
+                <p>Nome não encontrado</p>
+            </div>
+        )}
     </div>
   )
 }
