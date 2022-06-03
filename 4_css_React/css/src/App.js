2600f3a4cbcd2f11db022900f10cba8d32a39c5c
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2022-06-03 17:46:06 -0300

    Estilização de componentes utilizando classes no REACT

--- 4_css_React/css/src/App.js
+++ 4_css_React/css/src/App.js
@@ -1,10 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import MyComponent from './components/MyComponent';
 
 function App() {
   return (
     <div className="App">
+      {/* CSS Global */}
       <h1>css do React</h1>
+      {/* CSS de componentes */}
+      <MyComponent/>
+      <p>Este componente pertence ao App.css</p>
     </div>
   );
 }
---
+++ 4_css_React/css/src/components/MyComponent.js
@@ -0,0 +1,13 @@
+import "./MyComponent.css"
+
+const MyComponent = ()=>{
+
+    return(
+        <div>
+            <h1>css de componentes</h1>
+            <p className="warning">Este é meu componente</p>
+        </div>
+    )
+}
+
+export default MyComponent;
