e7e7cf08d099d117686202cf618cd813befa411b
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2022-06-06 15:15:14 -0300

    Estilo inline e inline Dinâmico com operador ternário

--- 4_css_React/css/src/App.js
+++ 4_css_React/css/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import MyComponent from './components/MyComponent';
 
 function App() {
+  const valor = 15;
   return (
     <div className="App">
       {/* CSS Global */}
@@ -9,6 +10,14 @@ function App() {
       {/* CSS de componentes */}
       <MyComponent/>
       <p>Este componente pertence ao App.css</p>
+      {/* CSS inline*/}
+      <p style={{color:"white", padding:"20px",fontSize:"25px"}}>
+        Este é o estilo inline
+      </p>
+      {/* CSS inline dinâmico */}
+      <p style={valor <= 15 ? {color:"blue",backgroundColor:"#2424"} : {color:"red"}}>
+        Este é um estilo inline Dinâmico
+      </p>
     </div>
   );
 }
