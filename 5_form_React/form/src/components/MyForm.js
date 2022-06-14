4b08e9b66a29701e9b95bc612350c5ccb08bd36f
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2022-06-14 16:11:24 -0300

    Gerenciando dados do input com useState

--- 5_form_React/form/src/components/MyForm.js
+++ 5_form_React/form/src/components/MyForm.js
@@ -1,13 +1,30 @@
 import styles from "./MyForm.css";
+import {useState} from "react";
 
 const MyForm = ()=>{
+
+    const [name, setName] = useState();
+    const [Email, setEmail] = useState();
+
+    const handleName = (e) => {
+        setName(e.target.value);
+    }
+
+    console.log(name);
 
     return(
         <>
             <form>
                 <div className="form-control">
                     <label htmlFor="name">Nome:</label>
-                    <input type="text" name="name" />
+                    <input onChange={handleName} type="text" name="name" placeholder="Digige seu nome"/>
+                </div>
+                {/* label envolvendo o input*/}
+                <div className="form-control">
+                    <label>
+                        <span>E-mail</span>
+                        <input type="e-mail" placeholder="Digite seu email"/>
+                    </label>
                 </div>
                 <div>
                     <input type="submit" value="Enviar"/>
