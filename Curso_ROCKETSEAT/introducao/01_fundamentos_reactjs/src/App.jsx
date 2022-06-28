25be806d74a063908b53f932c3c9652d96b5bf56
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2022-06-28 15:30:28 -0300

    CSS module aplica uma estilização única ao componente

--- Curso_ROCKETSEAT/introducao/01_fundamentos_reactjs/src/App.jsx
+++ Curso_ROCKETSEAT/introducao/01_fundamentos_reactjs/src/App.jsx
@@ -1,10 +1,13 @@
-import { Post } from "./Post"
+import { Header } from "./components/Header";
+import { Post } from "./Post";
+import "./styles.css";
 
 export function App() {
   
   return (
     
     <div>
+      <Header/>
       <Post 
         author="Jhonatan" 
         content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem explicabo sed nobis neque ducimus nulla architecto eius ipsam facilis rerum eos fuga, eligendi veritatis. Quaerat, dolor. Ad doloribus velit quasi!"
