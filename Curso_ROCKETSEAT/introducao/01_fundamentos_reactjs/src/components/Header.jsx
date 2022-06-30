2b6d56d0d63843f6e1b8f76ec9b568da6359b614
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2022-06-30 09:02:22 -0300

    Inserindo e centralizando a logo

--- Curso_ROCKETSEAT/introducao/01_fundamentos_reactjs/src/components/Header.jsx
+++ Curso_ROCKETSEAT/introducao/01_fundamentos_reactjs/src/components/Header.jsx
@@ -1,9 +1,11 @@
-import styles from "./Header.module.css"
+import styles from "./Header.module.css";
+
+import igniteLogo from "../assets/img/igniteLogo.svg"
 
 export function Header(){
    return(
         <header className={styles.header}>
-            <strong>Ignite</strong>
+            <img src={igniteLogo} alt="Logo do ignite" />
         </header>
     
    )
