48b87c2dbb452f49f2c1c2e2dd9228056d02e577
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2022-07-10 16:07:05 -0300

    Formulário de comentários

--- Curso_ROCKETSEAT/introducao/01_fundamentos_reactjs/src/components/Post.jsx
+++ Curso_ROCKETSEAT/introducao/01_fundamentos_reactjs/src/components/Post.jsx
@@ -25,6 +25,18 @@ export function Post(){
 
                 <p><a href="">#novoprojeto #nlw #rocketseat</a></p>
           </div>
+
+          <form className={styles.commentForm}>
+             <strong>Deixe seu comentário</strong>
+
+             <textarea 
+               placeholder="Deixe um comentário"
+             />
+
+             <footer>
+               <button type="submit">Publicar</button>
+             </footer>
+          </form>
         </article>
     )
 }
