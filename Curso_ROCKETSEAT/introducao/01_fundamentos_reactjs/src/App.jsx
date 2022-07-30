727b2e607bb63306c138d7de079f0e15954b6267
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2022-07-30 15:20:42 -0300

    Iterando no JSX com props

--- Curso_ROCKETSEAT/introducao/01_fundamentos_reactjs/src/App.jsx
+++ Curso_ROCKETSEAT/introducao/01_fundamentos_reactjs/src/App.jsx
@@ -6,9 +6,38 @@ import "./global.css";
 
 import styles from "./App.module.css"
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: "https://github.com/diego3g",
+      name: "Diego Fernandes",
+      role: "CTO @Rocketseat"
+    },
+    content: [
+      {type: "paragraph", content: "Fala galeraa 👋"},
+      {type: "paragraph", content: "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀"},
+      {type: "link", content: "👉 jane.design/doctorcare"},
+    ],
+    publishedAt: new Date("2022-06-10 14:00:55"),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: "https://github.com/JhonatanBS",
+      name: "Jhonatan Bezerra",
+      role: "Desenvolvedor Full-Stack"
+    },
+    content: [
+      {type: "paragraph", content: "Fala galeraa 👋"},
+      {type: "paragraph", content: "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀"},
+      {type: "link", content: "👉 jane.design/doctorcare"},
+    ],
+    publishedAt: new Date("2022-07-30 15:00:55"),
+  }
+];
 
 export function App() {
-  
   return (
     
     <div>
@@ -18,16 +47,17 @@ export function App() {
         <Sidebar/>
         
         <main>
-          <Post 
-            author="Jhonatan" 
-            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem explicabo sed nobis neque ducimus nulla architecto eius ipsam facilis rerum eos fuga, eligendi veritatis. Quaerat, dolor. Ad doloribus velit quasi!"
-            />
-
-            <Post 
-            author="Tiago" 
-            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem explicabo sed nobis neque ducimus nulla architecto eius ipsam facilis rerum eos fuga, eligendi veritatis. Quaerat, dolor. Ad doloribus velit quasi!"
-            />
-          </main>
+          {posts.map((post) => {
+            return (
+              <Post
+                key={post.id} 
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt} 
+              />
+            )
+          })}  
+        </main>
       </div>
       
     </div>
