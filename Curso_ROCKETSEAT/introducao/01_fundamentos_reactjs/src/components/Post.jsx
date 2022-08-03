692e43e036870fa24ae5cc050db6d2ba6df1bbf6
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2022-08-03 16:33:35 -0300

    Programação declarativa

--- Curso_ROCKETSEAT/introducao/01_fundamentos_reactjs/src/components/Post.jsx
+++ Curso_ROCKETSEAT/introducao/01_fundamentos_reactjs/src/components/Post.jsx
@@ -8,9 +8,10 @@ import styles from "./Post.module.css";
 
 export function Post({author, publishedAt, content}){
     const [comments, setComments] = useState([
-      1,
-      2,
+      "Post muito bacana, hein!?"
     ]);
+
+    const [newCommentText, setNewCommentText ] = useState("");
 
     const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'",
     { locale: ptBR});
@@ -23,7 +24,14 @@ export function Post({author, publishedAt, content}){
     function handleCreateNewComment() {
       event.preventDefault();
 
-      setComments([...comments, comments.length + 1]);
+      setComments([...comments, newCommentText]);
+
+      setNewCommentText("");
+
+    }
+
+    function handleNewCommentChange() {
+      setNewCommentText(event.target.value);
     }
 
     return (
@@ -56,7 +64,10 @@ export function Post({author, publishedAt, content}){
              <strong>Deixe seu comentário</strong>
 
              <textarea 
+               name="comment"
                placeholder="Deixe um comentário"
+               value={newCommentText}
+               onChange={handleNewCommentChange}
              />
 
              <footer>
@@ -66,7 +77,7 @@ export function Post({author, publishedAt, content}){
 
           <div className={styles.commentList}>
             {comments.map( comment => {
-              return <Comment />
+              return <Comment content={comment}/>
             })}
           </div>
         </article>
