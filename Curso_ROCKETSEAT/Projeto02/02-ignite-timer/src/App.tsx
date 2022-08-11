b52c8e5b5e06edf27f245c5010b73435644cd3d2
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2022-08-10 22:30:06 -0300

    styled-components

--- Curso_ROCKETSEAT/Projeto02/02-ignite-timer/src/App.tsx
+++ Curso_ROCKETSEAT/Projeto02/02-ignite-timer/src/App.tsx
@@ -1,7 +1,14 @@
+import { Button } from "./components/Button";
 
 export function App() {
   return (
-    <h1>Hello World</h1>
+    <>
+      <Button variant="primary"/>
+      <Button variant="secondary"/>
+      <Button variant="danger"/>
+      <Button variant="success"/>
+      <Button />
+    </>
   )
 }
 
