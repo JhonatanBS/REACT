6c998cde6fa5743e58477f7c3687c69e1eee3877
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2022-08-17 10:08:44 -0300

    Insert div todo empty

--- Desafios/todoList/src/App.tsx
+++ Desafios/todoList/src/App.tsx
@@ -1,11 +1,15 @@
 import { Header } from "./components/Header";
 import "./global.css";
 import styles from "./App.module.css";
+import clipboard from "./assets/img/Clipboard.svg";
 
-import {PlusCircle} from "phosphor-react";
+import {PlusCircle, ClipboardText} from "phosphor-react";
+import { useState } from "react";
+import { Todo } from "./components/Todo";
 
 export function App() {
-
+  const [todo, setTodo] = useState([]);
+  console.log(todo, setTodo)
   return (
     <>
       <Header/>
@@ -21,14 +25,33 @@ export function App() {
         <div className={styles.todoList}>
           <div className={styles.setupCount}>
             <div>
-              <p>Tarefas criadas <span>{0}</span></p>
+              <p>Tarefas criadas</p>
+              <div className={styles.number}>{0}</div>
             </div>
 
             <div>
-              <p>Concluídas <span>{0}</span></p>
+              <p>Concluídas</p>
+              <div className={styles.number}>{0}</div>
             </div>
           </div>
+          
+          <div className={styles.center}>
+            {todo.length === 0 ? (
+              <>
+                <div>
+                  <img src={clipboard} alt="" />
+                </div>
 
+                <div>
+                   <p>Você ainda não tem tarefas cadastradas</p>
+                   <p>Crie tarefas e organize seus itens a fazer</p>
+                </div>
+
+              </>
+            ) : (
+              <Todo />
+            )}
+          </div>
         </div>
       </main>
     </>
